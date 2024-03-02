2bd94590905771fb878f88cb1b9bce520af69276
RamsinChaabian <Ramsin.chaabian@gmail.com>
2024-03-02 14:05:35 +0330

    minified script.js

--- assets/js/shared-scripts.js
+++ assets/js/shared-scripts.js
@@ -2,7 +2,7 @@
 document.write('<script rel="preload" src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" as="script"></script>');
 
 // Custom JavaScript
-document.write('<script rel="preload" src="/assets/js/script.js" as="script"></script>');
+document.write('<script rel="preload" src="/assets/js/script.min.js" as="script"></script>');
 
 // Initial Settings JavaScript
 if (document.documentElement.lang != 'en') {
